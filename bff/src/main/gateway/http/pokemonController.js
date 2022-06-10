ed3c79c0e9b17d456846bce2546580acef0ff114
Renato Cabral Rodrigues de Souza <CIANDT\renatocrds@lnb035014blz.ciandt.global>
2022-06-10 15:25:10 -0300

    tratamento de dados do endpoint all

--- bff/src/main/gateway/http/pokemonController.js
+++ bff/src/main/gateway/http/pokemonController.js
@@ -1,4 +1,5 @@
 const Pokemon = require("../../domain/pokemon")
+const PokemonList = require("../../domain/pokemonList")
 const { default: axios } = require("axios")
 
 const API = "http://localhost:8081/pokemon/"
@@ -12,15 +13,28 @@ function createPokemon(data) {
         data.weight, 
         data.types, 
         data.abilities
-        )
-  }
+    )
+}
+
+function createPokemonList(data) {
+    let pokemonList = []
+    let imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"
+    for (const element of data) {
+        let name = element.name
+        let image = imageUrl.replace('0', element.url.split('/')[6])
+        let pokemon = new PokemonList(name, image)
+        pokemonList.push(pokemon)
+    }
+    return pokemonList
+}
 
 const pokemonController = {
     getAllPokemon: async (req, res) => {
         const url = API + "all"
         const response = await axios.get(url)
+        const pokemonList = createPokemonList(response.data['results'])
 
-        res.status(200).send(response.data)
+        res.status(200).send(pokemonList)
     },
 
     getSinglePokemon: async (req, res, nameOrId) => {
