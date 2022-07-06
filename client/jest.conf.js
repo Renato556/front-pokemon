ba3c786e08c82a6763768009238c7f24adc669bf
Renato Cabral Rodrigues de Souza <CIANDT\renatocrds@lnb035014blz.ciandt.global>
2022-07-06 10:22:22 -0300

    coverage no front

--- client/jest.conf.js
+++ client/jest.conf.js
@@ -1,61 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  rootDir: path.resolve(__dirname, './'),
-  testPathIgnorePatterns: [
-    '<rootDir>/client/node_modules/',
-    '<rootDir>/tests/',
-    '<rootDir>/public/',
-  ],
-  transformIgnorePatterns: [
-    'node_modules/(?!@b2b-canaisdigitais)(?!vee-validate)',
-  ],
-  modulePathIgnorePatterns: [
-  ],
-  verbose: true,
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/main/$1',
-    '\\.(jpg|jpeg|svg)$': '<rootDir>/src/spec/mocks/fileMock.js',
-  },
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.vue$': 'vue-jest',
-  },
-  globals: {
-    'vue-jest': {
-      babelConfig: {
-        presets: [
-          [
-            '@babel/preset-env',
-          ],
-        ],
-        plugins: [
-          'dynamic-import-node',
-          '@babel/plugin-syntax-dynamic-import',
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-transform-runtime',
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-transform-modules-commonjs',
-        ],
-      },
-    },
-  },
   collectCoverage: true,
-  coverageDirectory: process.env.JEST_CLOVER_OUTPUT_DIR || './coverage',
-  coverageReporters: ['text', 'clover', 'lcov'],
   collectCoverageFrom: [
-    '**/src/main/**/*.js',
-    '**/src/**/*.vue',
-    '**/src/**/**/*.vue',
-    '!**/src/main/*.js',
-    '!**/src/main/clients/*.js',
-    '!**/src/main/router/index.js',
-    '!**/src/main/plugins/*.js',
-    '!**/src/spec/**/*.js',
-    '!**/test/**/*.js',
+    'src/**/*.{js,vue}',
+    '!src/main.js', // No need to cover bootstrap file
   ],
   moduleFileExtensions: [
     'js',
-    'vue',
+    'jsx',
+    'json',
+    'vue'
   ],
-};
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost/'
+}
